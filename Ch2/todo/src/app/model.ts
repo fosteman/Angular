a3c67f2b21c7de482d6d14c2d3ad6f2cd159cd66
fosteman <dgrz5.0@gmail.com>
2019-05-17 04:16:23 -0400

    Clearence

--- Ch2/todo/src/app/model.ts
+++
@@ -1,8 +0,0 @@
-var model = {
-  user: "Adam",
-  items: [
-    { action: "Buy Flowers", done: false },
-    { action: "Get Shoes", done: false },
-    { action: "Collect Tickets", done: true },
-    { action: "Call Joe", done: false }]
-};
