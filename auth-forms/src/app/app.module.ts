59070d18ea06bf3027dee98f19393919efdb7ab9
fosteman <dgrz5.0@gmail.com>
2019-05-18 15:41:18 -0400

    Authentification using JWT RESTful backend service
    To test backend:
    curl -H "Content-Type: application/json" -d '{"email":"user@email.com","name":"user", "password":"password"}' http://localhost:3000/register
    curl -H "Content-Type: application/json" -d '{"email":"user@email.com", "password":"password"}' http://localhost:3000/login
    
    Angular directives such as ngModel, ngForm and ngSubmit

--- auth-forms/src/app/app.module.ts
+++ auth-forms/src/app/app.module.ts
@@ -1,15 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
+
+const  routes: Routes  = [
+  {path: 'auth', loadChildren: './auth/auth.module#AuthModule'}
+];
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule]
   providers: [],
   bootstrap: [AppComponent]
 })
