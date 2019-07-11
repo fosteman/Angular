5f08e79e13f055577e3995ad7c382f1c14ad6d67
Timofei Shchepkin <dgrz5.0@gmail.com>
2019-07-11 04:37:44 -0400

    used the CLI to create a second HeroesComponent displayed the HeroesComponent by adding it to the AppComponent shell. applied the UppercasePipe to format the name. used two-way data binding with the ngModel directive. earned about the AppModule. imported the FormsModule in the AppModule so that Angular would recognize and apply the ngModel directive. learned the importance of declaring components in the AppModule and appreciated that the CLI declared it for ME.

--- crisis-center/src/app/app.module.ts
+++ crisis-center/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppComponent } from './app.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroListComponent } from './hero-list/hero-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 
 const routes: Routes = [
   {path: 'crisis-center', component: CrisisListComponent},
-  {path: 'heroes', component: HeroListComponent },
+  {path: 'heroes-list', component: HeroListComponent },
+  {path: 'heroes-app', component: HeroesComponent},
   { path: '',   redirectTo: '/heroes', pathMatch: 'full' },
   {path: '**', component: PageNotFoundComponent}
 ];
@@ -21,10 +23,12 @@ const routes: Routes = [
     AppComponent,
     CrisisListComponent,
     HeroListComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(routes)
   ],
   providers: [],
